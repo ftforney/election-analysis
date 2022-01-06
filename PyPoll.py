2a9ee710ebceb5c420313f924b65ac19fee266a9
Frederick Forney <freddyforney0812@gmail.com>
2022-01-05 21:20:24 -0800

    Added some code | 3.5.2

--- PyPoll.py
+++ PyPoll.py
@@ -7,14 +7,21 @@
 
 import os
 import csv
-import datetime
+import datetime\
+
+total_votes = 0
+candidate_options = []
 
 read = os.path.join("Resources","election_results.csv")
 write = os.path.join("Analysis","election_analysis.txt")
 with open(read, 'r') as data:
     reader = csv.reader(data)
     headers = next(reader)
-    print(headers)
+    for row in reader:
+        candidate_name = row[2]
+        candidate_options.append(candidate_name) if candidate_name not in candidate_options else None
+        total_votes += 1
+print(candidate_options)
 
 # See if I can find a way to automatically create the directory and file if it does not exist.
 with open(write, 'w') as output:
