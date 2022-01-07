1b5ebe363de3cece0622f4c7d8f8b8f9cec63217
Frederick Forney <freddyforney0812@gmail.com>
2022-01-06 18:23:36 -0800

    Updated election audit code. Closer to completion.

--- PyPoll.py
+++ PyPoll.py
@@ -7,10 +7,14 @@
 
 import os
 import csv
-import datetime\
+import datetime
 
 total_votes = 0
 candidate_options = []
+candidate_votes = {}
+winning_candidate = ""
+winning_count = 0
+winning_percentage = 0
 
 read = os.path.join("Resources","election_results.csv")
 write = os.path.join("Analysis","election_analysis.txt")
@@ -19,9 +23,27 @@ with open(read, 'r') as data:
     headers = next(reader)
     for row in reader:
         candidate_name = row[2]
-        candidate_options.append(candidate_name) if candidate_name not in candidate_options else None
+        if candidate_name not in candidate_options:
+            candidate_options.append(candidate_name)
+            candidate_votes[candidate_name] = 0
+        candidate_votes[candidate_name]+=1
         total_votes += 1
-print(candidate_options)
+
+for key, value in candidate_votes.items():
+    votes=value
+    vote_percentage = value / total_votes * 100
+    print(f"{key}: {value / total_votes * 100:.1f}% ({value:,})\n")
+    if(votes>winning_count) and (vote_percentage>winning_percentage):
+        winning_count = votes
+        winning_percentage = vote_percentage
+        winning_candidate = key
+winning_candidate_summary = (
+    f"-------------------------\n"
+    f"Winner: {winning_candidate}\n"
+    f"Winning Vote Count: {winning_count:,}\n"
+    f"Winning Percentage: {winning_percentage:.1f}%\n"
+    f"-------------------------\n")
+print(winning_candidate_summary)
 
 # See if I can find a way to automatically create the directory and file if it does not exist.
 with open(write, 'w') as output:
