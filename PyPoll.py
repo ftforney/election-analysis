fdf258d14f6d62fb2cea80e8c669c42ba906d5f8
Frederick Forney <freddyforney0812@gmail.com>
2022-01-06 19:47:46 -0800

    Adding election analysis, as well as updated code.

--- PyPoll.py
+++ PyPoll.py
@@ -9,6 +9,9 @@ import os
 import csv
 import datetime
 
+#Defining variables we will need throughout the code here. Read is for the input file, Write is for the output file.
+read = os.path.join("Resources","election_results.csv")
+write = os.path.join("Analysis","election_analysis.txt")
 total_votes = 0
 candidate_options = []
 candidate_votes = {}
@@ -16,8 +19,7 @@ winning_candidate = ""
 winning_count = 0
 winning_percentage = 0
 
-read = os.path.join("Resources","election_results.csv")
-write = os.path.join("Analysis","election_analysis.txt")
+#Opening the data we want to read and looping through it, grabbing the candidate names and tallying their votes as we go through the whole file...
 with open(read, 'r') as data:
     reader = csv.reader(data)
     headers = next(reader)
@@ -29,22 +31,32 @@ with open(read, 'r') as data:
         candidate_votes[candidate_name]+=1
         total_votes += 1
 
-for key, value in candidate_votes.items():
-    votes=value
-    vote_percentage = value / total_votes * 100
-    print(f"{key}: {value / total_votes * 100:.1f}% ({value:,})\n")
-    if(votes>winning_count) and (vote_percentage>winning_percentage):
-        winning_count = votes
-        winning_percentage = vote_percentage
-        winning_candidate = key
-winning_candidate_summary = (
-    f"-------------------------\n"
-    f"Winner: {winning_candidate}\n"
-    f"Winning Vote Count: {winning_count:,}\n"
-    f"Winning Percentage: {winning_percentage:.1f}%\n"
-    f"-------------------------\n")
-print(winning_candidate_summary)
-
-# See if I can find a way to automatically create the directory and file if it does not exist.
+#Opening the file we want to output to and writing the relevant data.
 with open(write, 'w') as output:
-    output.write("Arapahoe\nDenver\nJefferson")
+    election_results = (
+        f"\nElection Results\n"
+        f"-------------------------\n"
+        f"Total Votes: {total_votes:,}\n"
+        f"-------------------------\n")
+    print(election_results, end="")
+    output.write(election_results)
+
+    #Looping through our candidate_votes dictionary so we can grab their vote count, and calculate a percentage...
+    for key, value in candidate_votes.items():
+        votes=value
+        vote_percentage = value / total_votes * 100
+        candidate_results = (f"{key}: {value / total_votes * 100:.1f}% ({value:,})\n")
+        print(candidate_results)
+        output.write(candidate_results)
+        if(votes>winning_count) and (vote_percentage>winning_percentage):
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winning_candidate = key
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+    print(winning_candidate_summary)
+    output.write(winning_candidate_summary)
