34ae1548151256eefe29207c0d0380ed70d71403
Frederick Forney <freddyforney0812@gmail.com>
2022-01-07 20:08:32 -0800

    Updated for challenge code portion.

--- PyPoll.py
+++ PyPoll.py
@@ -5,6 +5,13 @@
 # 4. Total num of votes each candidate won
 # 5. Winner of election based on popular vote
 
+# For the challenge:
+# 1. I need to get the voter turnout for each county.
+# 2. I need to get the percentage of votes from each county out of the total count.
+# 3. I need to get the county with the highest turnout.
+# In order to complete this, take a look at how we managed to get the basic voting data.
+# All we need to do is create a dictionary for a specified county, and the vote data.
+
 import os
 import csv
 import datetime
@@ -15,6 +22,11 @@ write = os.path.join("Analysis","election_analysis.txt")
 total_votes = 0
 candidate_options = []
 candidate_votes = {}
+counties = []
+county_vote_data = {}
+highest_county = ""
+highest_county_percentage = 0
+highest_county_count = 0
 winning_candidate = ""
 winning_count = 0
 winning_percentage = 0
@@ -25,10 +37,15 @@ with open(read, 'r') as data:
     headers = next(reader)
     for row in reader:
         candidate_name = row[2]
+        county = row[1]
         if candidate_name not in candidate_options:
             candidate_options.append(candidate_name)
             candidate_votes[candidate_name] = 0
-        candidate_votes[candidate_name]+=1
+        if county not in counties:
+            counties.append(county)
+            county_vote_data[county] = 0
+        candidate_votes[candidate_name] += 1
+        county_vote_data[county] += 1
         total_votes += 1
 
 #Opening the file we want to output to and writing the relevant data.
@@ -37,9 +54,28 @@ with open(write, 'w') as output:
         f"\nElection Results\n"
         f"-------------------------\n"
         f"Total Votes: {total_votes:,}\n"
-        f"-------------------------\n")
+        f"-------------------------\n\n"
+        f"County Votes:\n")
     print(election_results, end="")
     output.write(election_results)
+
+    #Looping through our county_vote_data dictionary so we can grab each counties vote count, and get a percentage...
+    for k,v in county_vote_data.items():
+        c_votes = v
+        c_vote_percentage = v/total_votes*100
+        county_results = (f"{k}: {v/total_votes*100:.1f}% ({v:,})\n")
+        print(county_results)
+        output.write(county_results)
+        if(c_votes>highest_county_count) and (c_vote_percentage>highest_county_percentage):
+            highest_county_count = c_votes
+            highest_county_percentage = c_vote_percentage
+            highest_county = k
+    highest_county_summary = (
+        f"\n-------------------------\n"
+        f"Largest County Turnout: {highest_county}\n"
+        f"-------------------------\n")
+    print(highest_county_summary)
+    output.write(highest_county_summary)
 
     #Looping through our candidate_votes dictionary so we can grab their vote count, and calculate a percentage...
     for key, value in candidate_votes.items():
